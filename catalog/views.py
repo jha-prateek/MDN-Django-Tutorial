78c009b9a26ad5b80b8bfb4bd7ae564fc5bc65b2
jha-pratik <prateekj777@gmail.com>
2020-04-29 01:17:39 +0530

    Completed all views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -18,3 +19,18 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    queryset = Book.objects.all()
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    queryset = Author.objects.all()
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
